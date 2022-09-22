49de837334829280252d6440cfaf55329360d3c9
nicolafan <nicola.fanelli16@gmail.com>
2022-09-22 10:08:35 +0200

    Change checkers format
    The format of the hands checker changes.
    An hand checker returns the hand in an ordered
    list where the first card is the strongest card
    of the hand (and so on, as the hand are explained
    on any website).
    This makes it possible to compare different hands
    with the kickers.
    
    None is returned if the hand is not present.

--- pyker/hands_checker.py
+++ pyker/hands_checker.py
@@ -1,29 +1,33 @@
 from pyker.entities import *
 
+
 def check_straight_flush(hand: Hand, community: Community):
     cards = hand.cards + community.cards
-    high_card = None
+    straight_flush = []
 
     for suit in Suit:
         same_suit_cards = [card for card in cards if card.suit == suit]
         same_suit_cards.sort()
         if len(same_suit_cards) >= 5:
-            previous_rank = same_suit_cards[0].rank
-            cnt = 1
+            straight_flush.append(same_suit_cards[0])
 
             for i in range(1, len(same_suit_cards)):
                 card = same_suit_cards[i]
-                if card.rank == previous_rank + 1:
-                    previous_rank += 1 
-                    cnt += 1
+                if card.rank == straight_flush[-1].rank + 1:
+                    straight_flush.append(card)
                 else:
-                    previous_rank = card.rank
-                    cnt = 1
-                
-                if cnt >= 5:
-                    high_card = card
+                    straight_flush = [card]
 
-                if i == len(same_suit_cards) - 1 and cnt >= 4 and previous_rank == 13 and any(card.rank == 1 for card in same_suit_cards):
-                    high_card = Card(suit, 1) 
-        
-    return high_card
+                if (
+                    i == len(same_suit_cards) - 1
+                    and len(straight_flush) >= 4
+                    and straight_flush[-1].rank == 13
+                    and any(card.rank == 1 for card in same_suit_cards)
+                ):
+                    straight_flush.append(Card(suit, 1))
+
+    if len(straight_flush) < 5:
+        return None
+
+    straight_flush.reverse()
+    return straight_flush[:5]
--- tests/test_hands_checker.py
+++ tests/test_hands_checker.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tests.util import *
+from tests.util import build_card as bc, build_hand as bh, build_community as bcm
 from pyker.hands_checker import *
 
 C = Suit.Clubs
@@ -13,19 +13,19 @@ S = Suit.Spades
     "hand,community,expected",
     [
         (
-            build_hand((C, 1), (C, 2)),
-            build_community([(C, 3), (D, 1), (C, 4), (C, 5), (C, 6)]),
-            build_card((C, 6)),
+            bh((C, 1), (C, 2)),
+            bcm([(C, 3), (D, 1), (C, 4), (C, 5), (C, 6)]),
+            [bc((C, 6)), bc((C, 5)), bc((C, 4)), bc((C, 3)), bc((C,2))],
         ),
         (
-            build_hand((H, 1), (D, 2)),
-            build_community([(S, 9), (S, 10), (S, 11), (S, 12), (S, 13)]),
-            build_card((S, 13)),
+            bh((H, 1), (D, 2)),
+            bcm([(S, 9), (S, 10), (S, 11), (S, 12), (S, 13)]),
+            [bc((S, 13)), bc((S, 12)), bc((S, 11)), bc((S, 10)), bc((S, 9))],
         ),
         (
-            build_hand((H, 10), (H, 1)),
-            build_community([(S, 9), (H, 11), (H, 12), (H, 13), (D, 2)]),
-            build_card((H, 1)),
+            bh((H, 10), (H, 1)),
+            bcm([(S, 9), (H, 11), (H, 12), (H, 13), (D, 2)]),
+            [bc((H, 1)), bc((H, 13)), bc((H, 12)), bc((H, 11)), bc((H, 10))],
         ),
     ],
 )
@@ -37,12 +37,12 @@ def test_check_straight_flush(hand, community, expected):
     "hand,community",
     [
         (
-            build_hand((C, 1), (C, 2)),
-            build_community([(C, 4), (D, 1), (C, 5), (C, 6), (C, 7)]),
+            bh((C, 1), (C, 2)),
+            bcm([(C, 4), (D, 1), (C, 5), (C, 6), (C, 7)]),
         ),
         (
-            build_hand((H, 2), (D, 3)),
-            build_community([(D, 4), (H, 10), (H, 11), (H, 12), (H, 13)]),
+            bh((H, 2), (D, 3)),
+            bcm([(D, 4), (H, 10), (H, 11), (H, 12), (H, 13)]),
         )
     ],
 )
